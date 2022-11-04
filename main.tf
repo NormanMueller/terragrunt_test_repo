0c2288958f986364913f174bc6a063f95016bc79
NormanMueller <93076064+NormanMueller@users.noreply.github.com>
2022-11-04 21:53:15 +0100

    add bucket test backend

--- main.tf
+++ main.tf
@@ -34,12 +34,3 @@ resource "aws_s3_bucket_public_access_block" "public_access" {
   restrict_public_buckets = true
 }
 
-resource "aws_dynamodb_table" "terraform_locks" {
-  name         = "terraform-up-and-running-locks"
-  billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "LockID"
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
-}
